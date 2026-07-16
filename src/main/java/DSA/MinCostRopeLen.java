085ca3e6290db72a4ce939fc69c01a0f4e86d835
stalindurai <stalindurai@gmail.com>
2026-07-16 16:53:43 +0100

    Implement findMinCost function and add test method
    Added a test method and implemented findMinCost function to calculate the minimum cost of connecting ropes.

--- src/main/java/DSA/MinCostRopeLen.java
+++ src/main/java/DSA/MinCostRopeLen.java
@@ -1,2 +1,26 @@
-package DSA;public class MinCostRopeLen {
+package DSA;
+
+public class MinCostRopeLen {
+
+  @Test
+	public static void Test1() {
+		int arr[] = {4,3,2,6};
+		int N = 4;
+		System.out.println(findMinCost(arr,N));
+	}
+	public static int findMinCost(int[] nums, int k) {
+		int sum = 0, cost = 0;
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i = 0; i < k; i++) {
+			pq.add(nums[i]);// 2,4,3,6
+		}
+		sum = pq.poll();
+		for (int i = 1; i < nums.length; i++) {
+			sum += pq.poll();
+			cost += sum;
+		}
+		return cost;
+		
+		
+	}
 }
