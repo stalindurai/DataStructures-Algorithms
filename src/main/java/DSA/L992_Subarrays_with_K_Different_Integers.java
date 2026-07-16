8e790da6815ef7df9655671f1e33e9a7e3d062e6
stalindurai <stalindurai@gmail.com>
2026-07-16 17:05:36 +0100

    Implement tests for findGoodSubArray functionality
    Added unit tests for findGoodSubArray method.

--- src/main/java/DSA/L992_Subarrays_with_K_Different_Integers.java
+++ src/main/java/DSA/L992_Subarrays_with_K_Different_Integers.java
@@ -1,2 +1,49 @@
-package DSA;public class L992_Subarrays_with_K_Different_Integers {
+package DSA;
+
+public class L992_Subarrays_with_K_Different_Integers {
+
+  public static void Test1() {
+		int[] inpArray = {1,2,1,2,3};
+		int k = 2;
+		System.out.println(findGoodSubArray(inpArray, k));
+	}
+	
+	@Test
+	public static void Test2() {
+		int[] inpArray = {1,2,1,3,4};
+		int k = 3;
+		System.out.println(findGoodSubArray(inpArray, k));
+	}
+	
+	
+	public static void Test3() {
+		int[] inpArray = {2,1,2,1,2};
+		int k = 2;
+		System.out.println(findGoodSubArray(inpArray, k));
+	}
+  
+  public static int findGoodSubArray(int[] nums, int k) {
+		
+		int count = 0, left = 0;
+		Map<Integer,Integer> map = new HashMap<>();
+		
+		
+		for(int right = 0;right<nums.length;right++) {
+			
+			map.put(nums[right], 0);
+			if(map.size()==k) {
+				count = count + 1;
+			}
+			else if ((map.size()>k)) {
+				map.clear();
+				right = left++;
+			}
+			if(right==nums.length-1 && (left-1)!=right) {
+				right = ++left;
+			}
+		}
+		
+		
+		return count;
+	}
 }
