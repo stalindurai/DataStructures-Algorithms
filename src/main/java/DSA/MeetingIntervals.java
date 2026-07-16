779a606949b0d3fc39efb56d3c5d6170ae8727ae
stalindurai <stalindurai@gmail.com>
2026-07-16 16:58:31 +0100

    Add unit tests for isAttendAllMeeting method
    Added unit tests for the isAttendAllMeeting method to verify its functionality with different meeting intervals.

--- src/main/java/DSA/MeetingIntervals.java
+++ src/main/java/DSA/MeetingIntervals.java
@@ -1,2 +1,49 @@
-package DSA;public class MeetingIntervals {
+package DSA;
+
+public class MeetingIntervals {
+
+  @Test
+	public static void Test1() {
+		int[][] inpArray =  {{0,30},{5,10},{15,20}};
+		System.out.println(isAttendAllMeeting(inpArray));
+	}
+	
+	@Test
+	public static void Test2() {
+		int[][] inpArray =  {{7,10},{2,4}};
+		System.out.println(isAttendAllMeeting(inpArray));
+	}
+	
+	@Test
+	public static void Test3() {
+		int[][] inpArray =  {{1,4},{2,3}};
+		System.out.println(isAttendAllMeeting(inpArray));
+	}
+
+  public static boolean isAttendAllMeeting(int[][] intervals) {
+		
+		Arrays.sort(intervals, (a,b)->a[0]-b[0]);
+	//	int starti = intervals[0][0];
+	//	int temps = intervals[0][0];
+		int endi = intervals[0][1];
+	//	int tempe = intervals[0][1];
+		
+		for(int i =1;i<intervals.length;i++) {
+			if(intervals[i][0] <= endi) {
+				return false;
+			}
+			else {
+			//	starti=intervals[i][0];
+				endi=intervals[i][1];
+			}
+		
+		}
+			
+		
+			return true;
+		
+		
+		
+		
+	}
 }
