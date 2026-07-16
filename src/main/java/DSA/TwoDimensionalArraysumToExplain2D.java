e536c0eb55517514a4e803d065b7d4990cbce30a
stalindurai <stalindurai@gmail.com>
2026-07-16 16:28:24 +0100

    Implement sum calculation for 2D array
    Added a method to calculate the sum of a 2D array and print it.

--- src/main/java/DSA/TwoDimensionalArraysumToExplain2D.java
+++ src/main/java/DSA/TwoDimensionalArraysumToExplain2D.java
@@ -1,2 +1,24 @@
-package DSA;public class TwoDimensionalArraysumToExplain2D {
+package DSA;
+
+public class TwoDimensionalArraysumToExplain2D {
+
+  @Test
+	public static void Test1() {
+		twoDArray();
+	}	
+	
+	public static void twoDArray() {
+	int[][] nums={{1,2,3},{4,5,6},{7,8,9}};
+    int sum=0;
+    // go over the rows
+    // O(n^2)
+    for (int i=0;i<nums.length;i++){
+        // go over the colums
+        for(int j=0;j<nums[0].length;j++){
+            sum+=nums[i][j];
+        }
+    }
+    System.out.println(Arrays.deepToString(nums));
+    System.out.println(sum);
+}
 }
