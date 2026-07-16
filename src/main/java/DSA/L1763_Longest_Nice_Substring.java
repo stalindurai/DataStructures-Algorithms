f9af6546a26a9f76e175d0abc1748fa9968e8454
stalindurai <stalindurai@gmail.com>
2026-07-16 21:58:40 +0100

    Implement tests for longestNiceSubstring method
    Added JUnit tests for the longestNiceSubstring method.

--- src/main/java/DSA/L1763_Longest_Nice_Substring.java
+++ src/main/java/DSA/L1763_Longest_Nice_Substring.java
@@ -1,2 +1,59 @@
-package DSA;public class L1763_Longest_Nice_Substring {
+package DSA;
+
+public class L1763_Longest_Nice_Substring {
+
+/*
+	 * Input : String
+	 * Output : String - return longest nice string
+	 * Test Data:
+	 * Pseudocode:
+	 * 
+	 * sliding window.
+	 * Start with k=3 size window. if all abc present, then calculate result by length - right pointer.
+	 * if not present then increase left pointer. and right pointer by one. until end of length. 
+	 * if all are present, then increase right pointer .
+	 * do this in for loop or while loop ,for each char in string
+	 * 
+	 * 
+	 * 
+	 */
+  
+  @Test
+public void Test1() {
+			String s= "YazaAay";
+			System.out.println(longestNiceSubstring(s));
+		}
+
+@Test
+public void Test2() {
+			String s= "Bb";
+			System.out.println(longestNiceSubstring(s));
+		}
+@Test
+public void Test3() {
+			String s= "c";
+			System.out.println(longestNiceSubstring(s));
+		}
+  
+  public String longestNiceSubstring(String s) {
+        if (s.length() < 2) return "";
+
+        // Check for characters that break the "nice" condition
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!s.contains(Character.toString(Character.toUpperCase(c))) || 
+                !s.contains(Character.toString(Character.toLowerCase(c)))) {
+                // Split and process both halves
+                String left = longestNiceSubstring(s.substring(0, i));
+                String right = longestNiceSubstring(s.substring(i + 1));
+                // Return the longer nice substring
+                return left.length() >= right.length() ? left : right;
+            }
+        }
+
+        // If no breaking character is found, the entire string is nice
+        return s;
+    }
+
+  
 }
