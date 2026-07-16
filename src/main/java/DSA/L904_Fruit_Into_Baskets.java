a1aaef3ef5a2a0218ce6ab497afed3408fac14ca
stalindurai <stalindurai@gmail.com>
2026-07-16 17:17:03 +0100

    Add unit tests for findMaxNumOfFruits method
    Added multiple test cases for the findMaxNumOfFruits method to validate its functionality with different fruit combinations.

--- src/main/java/DSA/L904_Fruit_Into_Baskets.java
+++ src/main/java/DSA/L904_Fruit_Into_Baskets.java
@@ -1,2 +1,85 @@
-package DSA;public class L904_Fruit_Into_Baskets {
+package DSA;
+
+public class L904_Fruit_Into_Baskets {
+
+  @Test
+	public static void Test1() {
+		int[] inpArray = {1,0,1,4,1,4,1,2,3};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+	@Test
+	public static void Test2() {
+		int[] inpArray = {1,2,1};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+	
+	@Test
+	public static void Test22() {
+		int[] inpArray = {0,1,2,2};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+	
+	@Test
+	public static void Test3() {
+		int[] inpArray = {1,2,3,2,2};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+	@Test
+	public static void Test4() {
+		int[] inpArray = {1,1};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+	@Test
+	public static void Test5() {
+		int[] inpArray = {1,2};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+	@Test
+	public static void Test6() {
+		int[] inpArray = {1};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+	@Test
+	public static void Test7() {
+		int[] inpArray = {1,2,3};
+		int result = findMaxNumOfFruits(inpArray);
+		System.out.println(result);
+	}
+
+  public static int findMaxNumOfFruits(int[] fruits) {
+		
+		if(fruits.length==0) {
+			return 0;
+		}
+		int maxLen = Integer.MIN_VALUE, left = 0;
+		Map<Integer,Integer> map = new HashMap<>();
+		
+		for(int idx = 0; idx < fruits.length;idx++) {
+			map.put(fruits[idx] ,map.getOrDefault(fruits[idx], 0) + 1);
+			
+			if(map.size() > 2) {
+								
+				int firstFruit = fruits[left];
+				map.put(firstFruit, map.get(firstFruit)-1);
+				if(map.get(firstFruit) == 0) {
+					
+					map.remove(firstFruit);
+				 
+			  }
+				left++;
+			}
+			int len = idx - left + 1;
+			maxLen = Math.max(maxLen, len);
+			
+		}
+				
+		return maxLen;
+	}
 }
