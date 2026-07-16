ab806f4d751548f7d2fad1850049d46bc07f7e71
stalindurai <stalindurai@gmail.com>
2026-07-16 17:04:09 +0100

    Implement longest substring without duplicates logic
    Added a test method and implemented logic to find the length of the longest substring without duplicate characters using ASCII values.

--- src/main/java/DSA/LongestSubStringWithoutDupsASCII.java
+++ src/main/java/DSA/LongestSubStringWithoutDupsASCII.java
@@ -1,2 +1,33 @@
-package DSA;public class LongestSubStringWithoutDupsASCII {
+package DSA;
+
+public class LongestSubStringWithoutDupsASCII {
+
+  @Test
+	public static void Test1() {
+		String inpStr = "abcabcdabc";
+		int maxlen = findMaxSubArrayNoDuplicate(inpStr);
+		System.out.println(maxlen);
+	}
+	
+	public static int findMaxSubArrayNoDuplicate(String s) {
+		int maxLen = Integer.MIN_VALUE, p1 = 0, p2 = 0;
+		
+	//	HashMap<Character,Integer> map = new HashMap<>();
+		int[] ascii = new int[128];
+		
+		
+		while(p2<s.length()) {
+			ascii[s.charAt(p2) - ' ']++;
+			//map.put(s.charAt(p2), map.getOrDefault(s.charAt(p2), 0) + 1);
+			while(ascii[s.charAt(p2) - ' '] > 1) {
+				ascii[s.charAt(p1) - ' ']--;
+				//map.put(s.charAt(p1), map.getOrDefault(s.charAt(p1), 0) - 1);
+				p1++;
+			}
+			maxLen = Math.max(maxLen, p2-p1+1);
+			p2++;
+		}
+		
+		return maxLen;
+	}
 }
