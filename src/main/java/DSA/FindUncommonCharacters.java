1834a689d1327dac6c16734d531703fa369dc355
stalindurai <stalindurai@gmail.com>
2026-07-16 22:21:39 +0100

    Implement uncommon character detection
    Added methods to test for uncommon characters between two strings.

--- src/main/java/DSA/FindUncommonCharacters.java
+++ src/main/java/DSA/FindUncommonCharacters.java
@@ -1,2 +1,40 @@
-package DSA;public class FindUncommonCharacters {
+package DSA;
+
+public class FindUncommonCharacters {
+
+  /*PsuedoCode:
+	 * From a to z check whether it is present in str1 and not in str2  or viceversa, print.
+	 *
+	 */
+	@Test
+	public static void Test1() {
+		String inpStr1 = "characters";
+		String inpStr2 = "alphabets";
+		//boolean result = checkForUncommonChars(inpStr);
+		checkForUncommonChars(inpStr1,inpStr2);
+		//System.out.println(result);
+	}
+	@Test
+	public static void Test2() {
+		String inpStr1 = "geeksforgeeks";
+		String inpStr2 = "geeksquiz";
+		//boolean result = checkForUncommonChars(inpStr);
+		checkForUncommonChars(inpStr1,inpStr2);
+		//System.out.println(result);
+	}
+	
+	
+	public static void checkForUncommonChars(String str1, String str2) {
+		
+		if(str1.equals(str2)) {
+			System.out.println(str1);
+		}
+		for(char c='a';c<='z';c++){
+            if((str1.indexOf(c)==-1 && str2.indexOf(c) > -1 )||(str2.indexOf(c)==-1 && str1.indexOf(c) > -1 )) {
+               System.out.print(c);
+            }
+        
+		}
+
+	}
 }
