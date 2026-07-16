16b0398a678fe0afd91bb734e6f040b005e468f3
stalindurai <stalindurai@gmail.com>
2026-07-16 17:39:43 +0100

    Add binary search implementation and test cases
    Implemented binary search algorithm to find the insert position of a target in a sorted array. Added test cases to validate the functionality.

--- src/main/java/DSA/L35_Search_Insert_Position.java
+++ src/main/java/DSA/L35_Search_Insert_Position.java
@@ -1,2 +1,62 @@
-package DSA;public class L35_Search_Insert_Position {
+package DSA;
+
+public class L35_Search_Insert_Position {
+
+  /*
+	Binary search:
+		- initialise two pointers low=0, high=nums.length-1
+		- while (low<=high)
+			- find the mid point
+			- if (nums[mid]==target) return mid;
+			- if <something> move left
+				high=mid-1
+			- if <something> move to right
+				low=mid+1;
+				
+				*/
+
+  @Test
+public static void Test1() {
+		int[] inputArr = {1,3,5,5,5,5,6};
+		int k = 5;
+		System.out.println(binarySearch(inputArr,k));
+	}
+	
+	@Test
+	public static void Test2() {
+		int[] inputArr = {1,3,5,5,5,5,6};
+		int k = 6;
+		System.out.println(binarySearch(inputArr,k));
+	}
+	
+	@Test
+	public static void Test3() {
+		int[] inputArr = {1,3,5,5,5,5,6};
+		int k = 7;
+		System.out.println(binarySearch(inputArr,k));
+	}
+
+  public static int binarySearch(int[] nums, int target){
+		  
+		  int result = nums.length, low = 0, high = nums.length-1;
+		  
+		  while(low <= high) {
+	    
+	    
+	       int  mid = (low + high)/2;
+	    
+	        if(nums[mid] >= target) {
+	        
+	            result = mid;
+	            high = mid - 1;
+	        }
+	        else if(nums[mid]< target){
+	        
+	            low = mid+1;
+	        }
+	    }
+	    
+	    return result;
+	    
+	}
 }
