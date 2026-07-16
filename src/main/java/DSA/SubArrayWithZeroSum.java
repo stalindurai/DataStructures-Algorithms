9eb01c891d164eb9aeca3415ef51c48205c275ef
stalindurai <stalindurai@gmail.com>
2026-07-16 16:33:32 +0100

    Implement test for zero-sum subarray
    Added a test method to check for subarray with zero sum.

--- src/main/java/DSA/SubArrayWithZeroSum.java
+++ src/main/java/DSA/SubArrayWithZeroSum.java
@@ -3,4 +3,35 @@ package DSA;
 //
 
 public class SubArrayWithZeroSum {
+
+  @Test
+	public static void Test4() {
+		int[] inputArray = {0};
+		
+		boolean isPresent = findsubArray0Sum(inputArray);
+		System.out.println(isPresent);
+		
+	}
+	
+	public static boolean findsubArray0Sum(int[] nums) {
+	 
+	int sum = 0;
+	
+	HashMap<Integer, Integer> map = new HashMap<>();
+	
+	map.put(0, 1);
+	
+	for(int i=0;i<nums.length;i++){
+		
+		sum += nums[i];
+		
+		if(map.containsKey(sum)) {
+			
+			return true;
+		}
+		map.put(sum, 1);
+
+		}
+		return false;
+}
 }
