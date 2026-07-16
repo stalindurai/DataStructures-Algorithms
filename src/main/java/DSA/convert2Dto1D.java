f20637dc56d53a620e28e447ffb8d0619e772573
stalindurai <stalindurai@gmail.com>
2026-07-16 15:13:47 +0100

    Add method to convert 2D array to 1D array

--- src/main/java/DSA/convert2Dto1D.java
+++ src/main/java/DSA/convert2Dto1D.java
@@ -1,2 +1,30 @@
-package DSA;public class convert2Dto1D {
+package DSA;
+
+public class convert2Dto1D {
+
+  @Test
+	public static void Test1() {
+		int[][] inpArray = {{1,2,3},{4,5,6},{7,8,9}};
+		System.out.println(Arrays.toString(convert2DTo1D(inpArray)));
+						
+		}
+	public static int[] convert2DTo1D(int[][] matrix) {
+		
+		if(matrix == null || matrix.length == 0) {
+			return new int[] {};
+		}
+		
+		int rows = matrix.length;
+		int columns = matrix[0].length;
+		int[] result = new int[rows*columns];
+		int index = 0;
+		
+		for(int i=0;i<rows;i++) {
+			for(int j = 0;j<columns;j++) {
+				result[index] =  matrix[i][j];
+				index++;
+			}
+		}
+		return result;
+	}
 }
