87475738c7c7c84f70c3f9bbdcb1d15e8e2ed3a6
stalindurai <stalindurai@gmail.com>
2026-07-16 16:30:00 +0100

    Implement water trapping algorithm with test case
    Added a test method and implemented the storeWater function to calculate trapped water using a two-pointer approach.

--- src/main/java/DSA/TrappingWaterwithTwoPointers.java
+++ src/main/java/DSA/TrappingWaterwithTwoPointers.java
@@ -1,2 +1,47 @@
-package DSA;public class TrappingWaterwithTwoPointers {
+package DSA;
+
+public class TrappingWaterwithTwoPointers {
+
+  @Test
+	public static void Test8() {
+		
+	
+		int[] inputArray = {0,1,0,2,1,0,1,3,2,1,2,1};
+		
+		int maxSum = storeWater(inputArray);
+		System.out.println(maxSum);
+		
+	}
+  
+  public static int storeWater(int[] nums) {
+	
+		int left = 0, right = nums.length-1, ans = 0;
+		int leftMax = Integer.MIN_VALUE; 
+		int rightMax = Integer.MIN_VALUE;
+		
+		while(left<=right) {
+			if(nums[left]<=nums[right]) {
+				if(nums[left]>=leftMax) {
+					leftMax = nums[left];
+				}
+					else {
+						ans += leftMax - nums[left];
+						
+				}
+				left++;
+			}
+			else {
+				if(nums[right]>=rightMax) {
+					rightMax = nums[right];
+				 }
+				else {
+					ans += rightMax - nums[right]; 
+				}
+				 right--;
+				}
+			
+			}
+		
+	return ans;
+}
 }
