49f2ee20a1ba0c2660ba38a141bad1123830bfed
stalindurai <stalindurai@gmail.com>
2026-07-16 22:16:42 +0100

    Add test and modifyQueue method for reversing elements
    Added a test method and implemented queue modification logic.

--- src/main/java/DSA/G_Reverse_First_K_elements_of_Queue.java
+++ src/main/java/DSA/G_Reverse_First_K_elements_of_Queue.java
@@ -1,2 +1,48 @@
-package DSA;public class G_Reverse_First_K_elements_of_Queue {
+package DSA;
+
+public class G_Reverse_First_K_elements_of_Queue {
+
+  @Test
+	public static void Test1() {
+		
+		int k = 3;
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(1);
+		queue.add(2);
+		queue.add(3);
+		queue.add(4);
+		queue.add(5);
+		System.out.println(modifyQueue(queue,k));
+		
+	}
+
+  public static Queue<Integer> modifyQueue(Queue<Integer> queue, int K){
+		
+		if (queue == null || K <= 0 || K > queue.size()) {
+			
+			return queue; 
+			
+			} 
+		Stack<Integer> stack = new Stack<>(); 
+		
+		for (int i = 0; i < K; i++) {
+			
+			stack.push(queue.remove()); 
+		} 
+		
+		while (!stack.isEmpty()) { 
+			
+			queue.add(stack.pop()); 
+		} 
+		
+		int size = queue.size(); 
+		
+		for (int i = 0; i < size - K; i++) {
+			
+			queue.add(queue.remove()); 
+			
+			} 
+		
+		return queue;
+	}
 }
