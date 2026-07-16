ad5592deef801077bb98cb3f0ef4aff03fe3adeb
stalindurai <stalindurai@gmail.com>
2026-07-16 17:32:47 +0100

    Implement singleNonDuplicate method with test case

--- src/main/java/DSA/L540_SingleElementinaSortedArray.java
+++ src/main/java/DSA/L540_SingleElementinaSortedArray.java
@@ -1,2 +1,38 @@
-package DSA;public class L540_SingleElementinaSortedArray {
+package DSA;
+
+public class L540_SingleElementinaSortedArray {
+
+  @Test
+    public void testOne()
+    {
+        int[] nums={1,1,2,3,3,4,4,8,8};
+        int i = singleNonDuplicate(nums);
+        System.out.println(i);
+    }
+
+  private int singleNonDuplicate(int[] nums) {
+
+        int low=1,high=nums.length-2,mid=0;
+        int n = nums.length;
+        if(n==1) return nums [0];
+        if (nums [0]!= nums [1]) 
+        	return nums [0]; 
+        if(nums [n-1] != nums[n-2]) 
+        	return nums[n-1];
+
+        
+        while(low<=high)
+        {  
+        	mid=(low+high)/2;
+        
+        	if ((mid%2==0 && nums[mid]==nums[mid+1])||(mid%2==1 && nums[mid]==nums[mid-1]))
+          {
+        	  low=mid+1; 
+          }
+          else {
+        	  high=mid-1;
+          }
+        }
+     return nums[low];
+    }
 }
