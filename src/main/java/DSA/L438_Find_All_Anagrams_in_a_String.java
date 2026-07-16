de56673089c1cbe1f04697a585d2e8277b026b7f
stalindurai <stalindurai@gmail.com>
2026-07-16 17:36:09 +0100

    Add tests and implement findAllAnagrams method

--- src/main/java/DSA/L438_Find_All_Anagrams_in_a_String.java
+++ src/main/java/DSA/L438_Find_All_Anagrams_in_a_String.java
@@ -1,2 +1,68 @@
-package DSA;public class L438_Find_All_Anagrams_in_a_String {
+package DSA;
+
+public class L438_Find_All_Anagrams_in_a_String {
+
+  @Test
+	public static void Test1() {
+		String inpStr1 = "cbaebabacd";
+		String inpStr2 = "abc";
+		System.out.println(findAllAnagrams(inpStr1,inpStr2));
+	}
+	@Test
+	public static void Test2() {
+		String inpStr1 = "abab";
+		String inpStr2 = "ab";
+		System.out.println(findAllAnagrams(inpStr1,inpStr2));
+	}
+
+  public static List<Integer> findAllAnagrams(String s,String p) {
+		
+
+		int p1 = 0, p2 = 0, count = p.length();
+		
+		 List<Integer> output = new ArrayList<>();
+		 
+		 int[] asciiCount = new int[26];
+		 
+		 for(char c: p.toCharArray()) {
+			 
+			 asciiCount[c-'a']++;
+			 
+		 }
+		 
+		 while(p2 < s.length()) {
+			 
+			 if(asciiCount[s.charAt(p2)-'a'] >= 1) {
+				 count--;
+						 
+			 }			 
+			 asciiCount[s.charAt(p2)-'a']--;
+			 
+			 p2++;
+		 
+			 if(count==0) {
+			 
+				 output.add(p1);
+		    }
+		 
+			 if (p2 - p1 == p.length()) { 
+				 
+			 char leftChar = s.charAt(p1);
+			 
+			 if (asciiCount[leftChar - 'a'] >= 0) { 
+				 
+				 count++;
+				 
+			 } 
+			 
+			 asciiCount[leftChar - 'a']++; 
+			 
+			 p1++; 
+			} 
+		 }
+	
+		 return output;
+    
+			
+	}
 }
