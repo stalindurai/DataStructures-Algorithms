fc1542495d9604bd042553c3f61cc2c88348b0be
stalindurai <stalindurai@gmail.com>
2026-07-16 17:08:19 +0100

    Add method to find squares of a sorted array
    Implemented a method to find squares of a sorted array using a two-pointer approach. Added test cases for validation.

--- src/main/java/DSA/L977_Squaresof_a_SortedArray.java
+++ src/main/java/DSA/L977_Squaresof_a_SortedArray.java
@@ -1,2 +1,56 @@
-package DSA;public class L977_Squaresof_a_SortedArray {
+package DSA;
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/description/
+	/*
+	 * Input: int array ( negative and positive numbers with duplicates)
+	 * Output: int array 
+	 * Test Data:
+	 * Pseudocode:
+	 * 
+	 * use two pointers
+	 * one left to 0 index and another right to nums.length-1 index
+	 * square it and compare both, if left is bigger write into an o/p array. if right is bigger write it to an output array. increment the pointer.
+	 * 
+	 * 
+	 * 
+	 */
+
+public class L977_Squaresof_a_SortedArray {
+
+  @Test
+	public static void Test1() {
+		int[] inpArray = {-4,-1,0,3,10};
+		System.out.println(Arrays.toString(findSquaresofnums(inpArray)));
+	}
+	
+	@Test
+	public static void Test2() {
+		int[] inpArray = {-7,-3,2,3,11};
+		System.out.println(Arrays.toString(findSquaresofnums(inpArray)));
+	}
+
+  public static int[] findSquaresofnums(int[] nums) {
+		
+		int left = 0, right = nums.length-1;
+		int index = nums.length-1;
+		int[] output = new int[nums.length];
+		
+		while(left<=right) {
+			if(nums[left]*nums[left]>=nums[right]*nums[right]) {
+				output[index] = nums[left]*nums[left];
+				left++;
+				index--;
+			}
+			else {
+				output[index] = nums[right]*nums[right];
+				right--;
+				index--;
+				
+			}
+		}
+		
+		
+		
+		return output;
+	}
 }
