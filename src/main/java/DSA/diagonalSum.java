c7d9baa49b0812df5a6ba366a582da640b50a8be
stalindurai <stalindurai@gmail.com>
2026-07-16 15:10:53 +0100

    Implement diagonal sum calculation for 2D array
    Added a method to calculate the diagonal sum of a 2D array and a test case to verify its functionality.

--- src/main/java/DSA/diagonalSum.java
+++ src/main/java/DSA/diagonalSum.java
@@ -1,2 +1,29 @@
-package DSA;public class diagonalSum {
+package DSA;
+
+public class diagonalSum {
+
+  @Test
+	public static void Test1() {
+		int[][] inputNums={{1,2,3},{4,5,6},{7,8,9}};
+		int diagonalSum = twoDArray(inputNums);
+		System.out.println(diagonalSum);
+	}	
+	
+
+	public static int twoDArray(int[][] nums) {
+	
+    int sum=0;
+    int n = nums.length;
+    
+    for (int i=0;i<n;i++){
+       
+            sum += nums[i][i] + nums[i][n-1-i];
+                       
+           }
+    if(n % 2 != 0) {
+    	sum = sum - nums[n/2][n/2];
+    }
+    System.out.println(Arrays.deepToString(nums));
+    
+    return sum;
 }
