2cd79460d1d38ae22c36883175b527fc382621f2
stalindurai <stalindurai@gmail.com>
2026-07-16 16:38:16 +0100

    Implement selection sort with test method
    Added a test method to demonstrate selection sort.

--- src/main/java/DSA/SelectionSortII.java
+++ src/main/java/DSA/SelectionSortII.java
@@ -1,2 +1,28 @@
-package DSA;public class SelectionSortII {
+package DSA;
+
+public class SelectionSortII {
+
+  @Test
+	public static void Test() {
+		int[] inpArray = {3,4,5,1,2};
+		System.out.println(Arrays.toString(selectionSort(inpArray)));
+	}
+	
+	public static int[] selectionSort(int[] nums){
+		
+        for (int i=0;i<nums.length;i++){
+        	
+            int minIndex=i;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]<nums[minIndex]){
+                    minIndex=j;
+                }
+            }
+            // swap i, minIndex
+            int temp= nums[i];
+            nums[i]=nums[minIndex];
+            nums[minIndex]=temp;
+        }
+        return nums;
+    }
 }
