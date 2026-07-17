504f6c59a69944291f8f5ba838535adfcf1f4225
stalindurai <stalindurai@gmail.com>
2026-07-17 11:50:27 +0100

    Add bubble sort implementation and test method

--- src/main/java/DSA/BubbleSort.java
+++ src/main/java/DSA/BubbleSort.java
@@ -1,2 +1,30 @@
-package DSA;public class BubbleSort {
+package DSA;
+
+public class BubbleSort {
+
+  @Test
+	public static void Test() {
+		int[] inpArray = {3,4,5,1,2};
+		System.out.println(Arrays.toString(bubbleSort(inpArray)));
+	}
+
+	    public static int[] bubbleSort(int[] nums){
+	        boolean swapped=false;
+	        for(int i=0;i<nums.length;i++){
+	            swapped=false;
+	        //    for(int j=0;j<nums.length-1;j++){
+	            for(int j=0;j<nums.length-i-1;j++){
+	                // if the left ele is greater than right ele
+	                if(nums[j]>nums[j+1]){
+	                    // swap
+	                    int temp= nums[j];
+	                    nums[j]=nums[j+1];
+	                    nums[j+1]=temp;
+	                    swapped=true;
+	                }
+	            }
+	            if(!swapped) break;
+	        }
+	        return nums;
+	    }
 }
